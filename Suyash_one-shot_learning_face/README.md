ceabac7d31d1945a6329cc6b92b9ae5a4112aadf
Suyash Sachdeva <suyashsachdeva2403@gmail.com>
2024-10-19 10:12:40 +0530

    Added Utility functions

--- Suyash_one-shot_learning_face/README.md
+++
@@ -1,7 +0,0 @@
-# One-shot Learning for faces:
-
-## Problem Statement
-The problem statement for this project is to develop a one-shot learning model which would be able differentiate and remember 100 different faces.
-
-## How does your work solve it
-This work will solve this by making deeper and better CNN architecture instead of the previous shallow architecture with a maximum memory of 10 faces.
---
+++ one-shot_learning/utility_functions.py
@@ -0,0 +1,42 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, n_layers: int, filters: int, kernel: int = 3, growth_factor: float = 2.0, 
+                 moment: float = 0.7, stride: bool = True, alpha: float = 0.03):
+        super(ConvBlock, self).__init__()
+        PADDING = (kernel - 1) // 2  # Padding based on the kernel size
+        self.stride = stride
+
+        # Batch normalization layers
+        self.norm = nn.ModuleList([nn.BatchNorm1d(num_features=filters, momentum=moment) for _ in range(n_layers)])
+        
+        # Convolutional layers
+        self.conv = nn.ModuleList([nn.Conv2d(filters, filters, kernel, padding=PADDING) for _ in range(n_layers - 1)])
+        
+        # Non-linear activation function
+        self.nlin = nn.LeakyReLU(alpha)
+
+        # Add final convolutional layer with stride or max pooling if applicable
+        if stride:
+            self.conv.append(nn.Conv2d(int(filters // growth_factor), filters, kernel, stride=2, padding=PADDING))
+        else:
+            self.conv.append(nn.Conv2d(int(filters // growth_factor), filters, kernel, padding=PADDING))
+            self.pool = nn.MaxPool2d(2, 2)  # Max pooling layer
+
+        self.conv.reverse()  # Reverse the convolution layers for the forward pass
+
+    def forward(self, x):
+        if not self.stride:
+            x = self.pool(x)
+        
+        # Pass through convolutional and normalization layers with activation
+        for conv, norm in zip(self.conv, self.norm):
+            x = self.nlin(norm(conv(x)))
+        return x
